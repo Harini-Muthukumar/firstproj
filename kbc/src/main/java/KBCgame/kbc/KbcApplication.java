2ca3781a35776e4020468688af6552c00b420a8e
HARINI <HARINI@DESKTOP-MTLOM0T>
2020-09-01 22:19:41 +0530

    initial

--- kbc/src/main/java/KBCgame/kbc/KbcApplication.java
+++ kbc/src/main/java/KBCgame/kbc/KbcApplication.java
@@ -2,7 +2,7 @@ package KBCgame.kbc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//Harini
 @SpringBootApplication
 public class KbcApplication {
 
